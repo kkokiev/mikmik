f409130604992e8c0fd261897faf351421a6cbbc
Kaeserkiev <kaeserkiev@gmail.com>
2016-03-19 09:48:58 +0200

    responsive fixes

--- product/js/app.js
+++ product/js/app.js
@@ -1,1 +1,163 @@
-window.console&&console.log||(console={log:function(){},debug:function(){},info:function(){},warn:function(){},error:function(){}}),$(function(){var e=$("#navbar-btn"),o=$(".menu");e.on("click",function(e){e.preventDefault(),o.hasClass("js-nav-open")?o.removeClass("js-nav-open").slideUp():o.addClass("js-nav-open").slideDown()}),$("#carousel-1 .owl-carousel").owlCarousel({rtl:!0,navContainer:"#carousel-1 .carousel__wrap",nav:!0,loop:!0,margin:18,responsive:{0:{items:1},600:{items:2},800:{items:3},900:{items:4}}}),$("#carousel-2 .owl-carousel").owlCarousel({rtl:!0,navContainer:"#carousel-2 .carousel__wrap",nav:!0,loop:!0,margin:18,responsive:{0:{items:1},600:{items:2},800:{items:3},900:{items:4}}}),$("#carousel-3 .owl-carousel").owlCarousel({rtl:!0,navContainer:"#carousel-3 .carousel__wrap",nav:!0,loop:!0,margin:18,responsive:{0:{items:1},600:{items:2},800:{items:3},900:{items:4}}}),$("#carousel-4 .owl-carousel").owlCarousel({rtl:!0,navContainer:"#carousel-4 .carousel__wrap",nav:!0,loop:!0,margin:18,responsive:{0:{items:1},600:{items:2},800:{items:3},900:{items:4}}}),$("#supplier .owl-carousel").owlCarousel({rtl:!0,navContainer:"#supplier .supplier__carousel-wrap",nav:!0,loop:!0,margin:100,responsive:{0:{items:1},400:{items:2},600:{items:3},800:{items:4},960:{items:5}}})});
+//Common scripts
+
+
+//ie fix
+if(!(window.console && console.log)) {
+	console = {
+		log: function(){},
+		debug: function(){},
+		info: function(){},
+		warn: function(){},
+		error: function(){}
+	};
+}
+
+$(function () {
+
+	//setup navbar
+	var $navBtn = $('#navbar-btn'),
+		$navMenu = $('.menu');
+
+	$navBtn.on('click', function(event) {
+		event.preventDefault();
+
+		if( $navMenu.hasClass('js-nav-open')) {
+			$navMenu.removeClass('js-nav-open').slideUp();
+		} else {
+			$navMenu.addClass('js-nav-open').slideDown();
+		}
+	});
+
+
+	//setup product and supllier carousels
+	$('#carousel-1 .owl-carousel').owlCarousel({
+		rtl: true,
+		navContainer: '#carousel-1 .carousel__wrap',
+		nav: true,
+		loop: true,
+		margin: 18,
+		responsive: {
+			0: {
+				items: 1
+			},
+
+			600: {
+				items: 2
+			},
+
+			800: {
+				items: 3
+			},
+
+			900: {
+				items: 4
+			}
+		}
+	});
+
+	$('#carousel-2 .owl-carousel').owlCarousel({
+		rtl: true,
+		navContainer: '#carousel-2 .carousel__wrap',
+		nav: true,
+		loop: true,
+		margin: 18,
+		responsive: {
+			0: {
+				items: 1
+			},
+
+			600: {
+				items: 2
+			},
+
+			800: {
+				items: 3
+			},
+
+			900: {
+				items: 4
+			}
+		}
+	});
+
+	$('#carousel-3 .owl-carousel').owlCarousel({
+		rtl: true,
+		navContainer: '#carousel-3 .carousel__wrap',
+		nav: true,
+		loop: true,
+		margin: 18,
+		responsive: {
+			0: {
+				items: 1
+			},
+
+			600: {
+				items: 2
+			},
+
+			800: {
+				items: 3
+			},
+
+			900: {
+				items: 4
+			}
+		}
+	});
+
+	$('#carousel-4 .owl-carousel').owlCarousel({
+		rtl: true,
+		navContainer: '#carousel-4 .carousel__wrap',
+		nav: true,
+		loop: true,
+		margin: 18,
+		responsive: {
+			0: {
+				items: 1
+			},
+
+			600: {
+				items: 2
+			},
+
+			800: {
+				items: 3
+			},
+
+			900: {
+				items: 4
+			}
+		}
+	});
+
+	$('#supplier .owl-carousel').owlCarousel({
+		rtl: true,
+		navContainer: '#supplier .supplier__carousel-wrap',
+		nav: true,
+		loop: true,
+		margin: 100,
+		responsive: {
+			0: {
+				items: 1
+			},
+
+			400: {
+				items: 2
+			},
+
+			600: {
+				items: 3
+			},
+
+			800: {
+				items: 4
+			},
+
+			960: {
+				items: 5
+			}
+		}
+	});
+
+});
+
